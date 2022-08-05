683abf3e26d4cd653e75cdb0937a0ca5c4811683
implodingduck <zsek320@gmail.com>
2022-08-05 14:28:38 -0500

    trying to add debug

--- terraform/main.tf
+++ terraform/main.tf
@@ -368,7 +368,7 @@ resource "null_resource" "publish_func" {
   }
   provisioner "local-exec" {
     working_dir = "../func"
-    command     = "func azure functionapp publish ${azurerm_linux_function_app.func.name} --build remote"
+    command     = "CLI_DEBUG=1 && func azure functionapp publish ${azurerm_linux_function_app.func.name} --build remote"
     
   }
 }
