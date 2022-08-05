89deb565872a4a28186bb89f5f0a8caea3753c24
implodingduck <zsek320@gmail.com>
2022-08-05 10:46:14 -0500

    can we publish with azure file connection string

--- terraform/main.tf
+++ terraform/main.tf
@@ -333,6 +333,7 @@ resource "azurerm_linux_function_app" "func" {
   }
   app_settings = {
     "APPINSIGHTS_INSTRUMENTATIONKEY"               = azurerm_application_insights.app.instrumentation_key
+    "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING"     = "@Microsoft.KeyVault(SecretUri=https://${azurerm_key_vault.kv.name}.vault.azure.net/secrets/${azurerm_key_vault_secret.saconnstr.name}/)" #VaultName=${azurerm_key_vault.kv.name};SecretName=${azurerm_key_vault_secret.saconnstr.name})"
   }
 
 }
