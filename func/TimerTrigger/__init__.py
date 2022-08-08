5490c20bbcf3e8b44db9091f5c2c0300a203e60b
implodingduck <zsek320@gmail.com>
2022-08-08 10:20:54 -0500

    adding Authentication=ActiveDirectoryMsi

--- func/TimerTrigger/__init__.py
+++ func/TimerTrigger/__init__.py
@@ -45,7 +45,7 @@ def main(mytimer: func.TimerRequest) -> None:
     
     driver= '{ODBC Driver 17 for SQL Server}'
 
-    with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';') as conn:
+    with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';Authentication=ActiveDirectoryMsi;') as conn:
         with conn.cursor() as cursor:
             cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
             row = cursor.fetchone()
